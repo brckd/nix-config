b56a5f293d4545061ac3a76cd76ed6ceb47568a6
bricked <spider@bricked.dev>
2025-12-11 02:53:49 +0100

    modules/helix: update config

--- modules/helix/home.nix
+++ modules/helix/home.nix
@@ -3,25 +3,27 @@
   lib,
   pkgs,
   ...
-}:
-with lib; let
+}: let
   cfg = config.programs.helix;
 in {
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     programs.helix = {
       settings = {
         editor = {
           auto-format = false;
           cursor-shape.insert = "bar";
           file-picker.hidden = false;
+          line-number = "relative";
+          soft-wrap.enable = true;
           whitespace.render.tab = "all";
         };
       };
 
       languages = {
         language-server = {
+          # Astro language server
           astro-ls = {
-            command = "astro-ls";
+            command = "${pkgs.astro-language-server}/bin/astro-ls";
             args = ["--stdio"];
             config = {
               typescript.tsdk = "${pkgs.typescript}/lib/node_modules/typescript/lib";
@@ -29,107 +31,77 @@ in {
             };
           };
 
+          # Bash language server
+          bash-language-server.command = "${pkgs.bash-language-server}/bin/bash-language-server";
+
+          # Fish language server
+          fish-lsp.command = "${pkgs.fish-lsp}/bin/fish-lsp";
+
+          # Markdown language server
+          marksman.command = "${pkgs.marksman}/bin/marksman";
+
+          # Nix language server
+          nixd.command = "${pkgs.nixd}/bin/nixd";
+
+          # Python language server
+          pylsp.command = "${pkgs.python313Packages.python-lsp-server}/bin/pylsp";
+
+          # Rust language server
+          rust-analyzer = {
+            command = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+            # Rust linter
+            config.check.command = "${pkgs.clippy}/bin/clippy";
+          };
+
+          # TOML language server
+          tombi.command = "${pkgs.tombi}/bin/tombi";
+
+          # Typst language server
+          tinymist.command = "${pkgs.tinymist}/bin/tinymist";
+
+          # TypeScript language server
+          typescript-language-server.command = "${pkgs.typescript-language-server}/bin/typescript-language-server";
+
+          # CSS language server
+          vscode-css-language-server.command = "${pkgs.vscode-langservers-extracted}/bin/vscode-css-language-server";
+
+          # ESLint language server
+          vscode-eslint-language-server.command = "${pkgs.vscode-langservers-extracted}/bin/vscode-eslint-language-server";
+
+          # HTML language server
+          vscode-html-language-server.command = "${pkgs.vscode-langservers-extracted}/bin/vscode-html-language-server";
+
+          # JSON language server
           vscode-json-language-server = {
+            command = "${pkgs.vscode-langservers-extracted}/bin/vscode-json-language-server";
             config.json.schemas = [
               {
-                fileMatch = ["tsconfig.json"];
-                url = "https://json.schemastore.org/tsconfig";
+                fileMatch = ["package.json"];
+                url = "https://www.schemastore.org/package.json";
+              }
+              {
+                fileMatch = ["tsconfig*.json"];
+                url = "https://www.schemastore.org/tsconfig.json";
               }
             ];
           };
 
-          nixd = {
-            command = "nixd";
-          };
+          # YAML language server
+          yaml-language-server.command = "${pkgs.yaml-language-server}/bin/yaml-language-server";
 
-          rust-analyzer = {
-            config.check.command = "clippy";
-          };
-
-          tinymist = {
-            command = "tinymist";
-          };
-
-          yaml-language-server = {
-            config.yaml.schemas = {
-              "https://json.schemastore.org/github-workflow.json" = ".github/workflows/*.{yml, yaml}";
-            };
+          # Zig language server
+          zls = {
+            command = "${pkgs.zls}/bin/zls";
+            # Zig linter
+            config.check.command = "${pkgs.zig-zlint}/bin/zlint";
           };
         };
 
-        language =
-          [
-            {
-              name = "astro";
-              language-servers = ["astro-ls"];
-            }
-            {
-              name = "nix";
-              language-servers = ["nixd"];
-              formatter.command = "alejandra";
-            }
-            {
-              name = "toml";
-              formatter = {
-                command = "taplo";
-                args = ["fmt" "-"];
-              };
-            }
-            {
-              name = "typst";
-              language-servers = ["tinymist"];
-              formatter.command = "typstyle";
-            }
-          ]
-          ++ map (
-            {
-              name,
-              extension ? name,
-            }: {
-              inherit name;
-              formatter = {
-                command = "biome";
-                args = ["format" "--stdin-file-path" "stdin.${extension}" "--indent-style" "space"];
-              };
-            }
-          ) [
-            {
-              name = "javascript";
-              extension = "js";
-            }
-            {
-              name = "typescript";
-              extension = "ts";
-            }
-            {
-              name = "jsx";
-            }
-            {
-              name = "tsx";
-            }
-            {
-              name = "json";
-            }
-          ];
+        language = map (name: {
+          inherit name;
+          language-servers = ["typescript-language-server" "vscode-eslint-language-server"];
+        }) ["javascript" "jsx" "typescript" "tsx"];
       };
     };
-
-    home.packages = with pkgs; [
-      astro-language-server
-      vscode-langservers-extracted # HTML, CSS, JSON
-      marksman # Markdown
-      nixd
-      alejandra
-      python313Packages.python-lsp-server
-      rust-analyzer
-      clippy # Rust linter
-      taplo # TOML formatter
-      tinymist # Typst
-      typstyle
-      typescript
-      typescript-language-server
-      biome
-      yaml-language-server
-    ];
   };
 }
