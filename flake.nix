f564bb04d1fdd506671e7f64906aa0ec80767a33
bricked <spider@bricked.dev>
2025-12-11 15:33:39 +0100

    flake: prioritize nixos cache

--- flake.nix
+++ flake.nix
@@ -56,15 +56,15 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://bricked.cachix.org"
       "https://cache.nixos.org"
+      "https://bricked.cachix.org"
       "https://nix-community.cachix.org"
       "https://pre-commit-hooks.cachix.org"
       "https://statix.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "bricked.cachix.org-1:SPpNjrCYzlfisekwWTN7dEUQs+OrirrM92h1ZoEnciY="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "bricked.cachix.org-1:SPpNjrCYzlfisekwWTN7dEUQs+OrirrM92h1ZoEnciY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
       "statix.cachix.org-1:Z9E/g1YjCjU117QOOt07OjhljCoRZddiAm4VVESvais="
